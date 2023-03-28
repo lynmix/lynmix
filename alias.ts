1f6b9fd8ef4fc1f27fe5441d86906861905a07ba
Zack <lorenx@163.com>
2023-03-28 21:06:58 +0800

    chore: add aliases for Lynmix packages

--- alias.ts
+++ alias.ts
@@ -4,4 +4,14 @@ const r = (p: string) => resolve(__dirname, p);
 
 export const alias: Record<string, string> = {
   lynmix: r('./packages/lynmix/src/'),
+  '@lynmix/cli': r('./packages/cli/src/'),
+  '@lynmix/core': r('./packages/core/src/'),
+  '@lynmix/react': r('./packages/react/src/'),
+  '@lynmix/vue': r('./packages/vue/src/'),
+  '@lynmix/solid': r('./packages/solid/src/'),
+  '@lynmix/node': r('./packages/node/src/'),
+  '@lynmix/serve': r('./packages/serve/src/'),
+  '@lynmix/runtime': r('./packages/runtime/src/'),
+  '@lynmix/express': r('./packages/express/src/'),
+  'create-lynmix': r('./packages/create-lynmix/src/'),
 };
